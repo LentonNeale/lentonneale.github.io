503491c16b24c9acf9716ed07339ab02f4f0b4ae
LentonNeale <lentonneale@gmail.com>
2024-03-09 16:16:46 +1300

    removed more files from src directory and edited the requied files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-          Lenton has edited this line, and again?
-        </p>
-        <a
-          className="App-link"
-          href="www.stuff.co.nz"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
