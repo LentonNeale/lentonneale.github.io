5bf1a99aaf94dc599f37b2123d8465ecb2170b1c
mrlongislandicet <lentonneale@gmail.com>
2024-03-18 18:22:50 +1300

    Added collapse button and function to sidebars

--- src/components/layout_components/sidebars/LeftSidebar.js
+++ src/components/layout_components/sidebars/LeftSidebar.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Sidebar, Menu, SubMenu, MenuItem } from 'react-pro-sidebar';
-import { useTheme } from  '../../theme/ThemeContext'; // Ensure this path is correct
+import { useTheme } from '../../theme/ThemeContext'; // Ensure this path is correct
 import './sidebar.css';
 
 const LeftSidebar = ({ dynamicLinks }) => {
   const location = useLocation();
   const { theme, toggleTheme } = useTheme(); // Destructure to get the current theme
+  const [collapsed, setCollapsed] = useState(false);
 
+  const handleToggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
   useEffect(() => {
     // Effect to log path changes or dynamicLinks updates
   }, [location.pathname, dynamicLinks]);
@@ -15,19 +19,26 @@ const LeftSidebar = ({ dynamicLinks }) => {
   return (
     // Apply dynamic theme className to adjust styles based on the current theme
     <aside className='left-sidebar' data-theme={theme} aria-label="Sidebar Navigation">
-      <h1 className='h1-sidebar'>Left Sidebar</h1>
-      <Sidebar>
-        <Menu >
-          {/* <SubMenu label="Theme"> */}
-            <MenuItem className='toggle_button' onClick={toggleTheme}>Toggle Theme</MenuItem>
-          {/* </SubMenu> */}
-          {dynamicLinks.map((link) => (
-            <MenuItem key={link.to}>
-              <Link to={link.to}>{link.label}</Link>
-            </MenuItem>
-          ))}
-        </Menu>
-      </Sidebar>
+      <button className='sidebar-button' onClick={handleToggleSidebar}>
+        {collapsed ? '<' : '>'}
+      </button>
+      {collapsed && (
+        <div className='left-sidebar-container'>
+          <h1 className='h1-sidebar'>Left Sidebar</h1>
+          <Sidebar>
+            <Menu >
+              {/* <SubMenu label="Theme"> */}
+              <MenuItem className='toggle_button' onClick={toggleTheme}>Toggle Theme</MenuItem>
+              {/* </SubMenu> */}
+              {dynamicLinks.map((link) => (
+                <MenuItem key={link.to}>
+                  <Link to={link.to}>{link.label}</Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Sidebar>
+        </div>
+      )}
     </aside>
   );
 };
--- src/components/layout_components/sidebars/RightSidebar.js
+++ src/components/layout_components/sidebars/RightSidebar.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { useTheme } from  '../../theme/ThemeContext';
+import React, { useState } from 'react';
+import { useTheme } from '../../theme/ThemeContext';
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import './sidebar.css'
 
 const RightColumn = () => {
-  const  {theme}  = useTheme();
-  return(
-  <aside className='right-sidebar' data-theme={theme}> 
-    <h1 className='h1-sidebar'> Right SideBar </h1>
-  </aside>
+  const { theme } = useTheme();
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleToggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
+  return (
+    <aside className='right-sidebar' data-theme={theme} aria-label="Sidebar Navigation">
+      <button className='sidebar-button' onClick={handleToggleSidebar}>
+        {collapsed ? '>' : '<'}
+      </button>
+      {collapsed && (
+        <div className='right-sidebar-container'>
+          <h1 className='h1-sidebar'> Right SideBar </h1>
+          <Sidebar collapsed={collapsed}>
+            <Menu>
+
+            </Menu>
+          </Sidebar>
+        </div>
+      )}
+    </aside>
   );
 };
 
