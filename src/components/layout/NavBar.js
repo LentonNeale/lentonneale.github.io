5fb7c89dd179a25388189c1cdf4316c14e454ec7
LentonNeale <lentonneale@gmail.com>
2024-03-28 21:18:58 +1300

    Added react-bootstrap-icons

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,18 +1,18 @@
 import React from "react";
-import {useTheme } from '../theme/ThemeProvider';
+import { useTheme } from '../theme/ThemeProvider';
 import { NavLink } from "react-router-dom";
-
+import { MoonFill, SunFill } from "react-bootstrap-icons"
 
 
 const NavBar = () => {
-    const {theme, toggleTheme} = useTheme();
+    const { theme, toggleTheme } = useTheme();
     console.log("Theme:", theme); // Log the theme for debugging purposes
 
 
     return (
-        <div className={`navbar navbar-expand-lg ${theme === 'light' ? "navbar-light bg-light" : "navbar-dark bg-dark" }`}>
+        <div className={`navbar navbar-expand-lg ${theme === 'light' ? "navbar-light bg-light" : "navbar-dark bg-dark"}`}>
             <div className="container-fluid">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                         <NavLink className="nav-link" to="/">Home</NavLink>
                     </li>
@@ -23,7 +23,9 @@ const NavBar = () => {
                         <NavLink className="nav-link" to="/contact">Contact</NavLink>
                     </li>
                 </ul>
-                <button onClick={toggleTheme}>Toggle Theme</button> {/* Toggle button */}
+                <button className={`btn btn-transparent`} onClick={toggleTheme}>
+                {theme === 'light' ? <MoonFill color="yellow" /> : <SunFill color="yellow" />}
+                </button>
             </div>
         </div>
     );
